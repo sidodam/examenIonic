04537949b1d84b759662ec5df58e578af69dc10a
sido27 <sidokouki1@gmail.com>
2023-02-22 12:22:22 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'examen',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
